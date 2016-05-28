f9a7de0274e9fb4d80e1d21b224cff528d5a6013
Michael O'Connor <mkoconnor@gmail.com>
2016-05-27 23:45:33 -0400

    commit

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,19 @@ module Main where
 
 import Lib
 import Diagrams.Prelude
--- import Diagrams.Backend.SVG (loadImageSVG)
+import Diagrams.Backend.SVG
 import Diagrams.Backend.SVG.CmdLine
 
 -- example :: Diagram B
 -- example = unitSquare # fc blue ||| strut (1/4) ||| circle (1/2) # fc yellow
--- 
+--
 -- example :: Diagram B
 
 dollar = do
    result <- loadImageEmb "/Users/michaeloconnor/Downloads/100-dollar-bill.png"
    return $ case result of
      Left err -> error err
-     Right x -> image x # scale 0.1
+     Right x -> image x # scale 0.05 # rotate (45 @@ deg)
 
 -- dollar = loadImageSVG "/Users/michaeloconnor/Downloads/banknote.svg"
 
@@ -29,7 +29,6 @@ example1 = do
    banknote <- dollar
    return $ example 5 (banknote :: Diagram B)
 
-
 example2 :: Diagram B
 example2 = regPoly 7 4 # lc blue # fc yellow
 
@@ -37,4 +36,6 @@ spiderWeb :: Diagram B
 spiderWeb = mconcat (map (regPoly 7) [1..5])
 
 main :: IO ()
-main = mainWith example1
+main = do
+  dollar <- dollar
+  renderSVG "/tmp/foo.svg" (dims (r2 (600,400))) $ (dollar <> regPoly 7 3 # fc blue) ||| fromOffsets [r2 (1,1),r2 (1,0),r2 (1/2,1/2)] # strokeLine # lc black # lwL 0.3
