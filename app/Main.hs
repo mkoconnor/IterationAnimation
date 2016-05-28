6cac4a55c334ac3162139e1eff54f15be2c2af7b
Michael O'Connor <mkoconnor@gmail.com>
2016-05-27 20:11:12 -0400

    commit

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
 import Lib
+import Diagrams.Prelude
+import Diagrams.Backend.SVG.CmdLine
 
+example :: Diagram B
+example = ((circle 2 # showEnvelope) ||| (pentagon 3 # showEnvelope)) # showOrigin
+  
 main :: IO ()
-main = someFunc
+main = mainWith example
