6bdc6878c5ea0504ebbd808ecda4ac90417a8c8a
Michael O'Connor <mkoconnor@gmail.com>
2016-05-28 08:51:55 -0400

    commit

--- app/run.sh
+++ app/run.sh
@@ -2,7 +2,8 @@
 set -e -x
 
 rm -rf /tmp/foo*svg
+rm /tmp/out.mp4
 stack build
 stack exec IterationAnimation-exe
-parallel 'inkscape {} --export-png {.}.png' ::: /tmp/foo*svg
-ffmpeg -i /tmp/foo%03d.png -c:v libx264 -r 30 -pix_fmt yuv420p /tmp/out.mp4
+parallel 'inkscape {} --export-dpi=270 --export-png {.}.png' ::: /tmp/foo*svg
+ffmpeg -i /tmp/foo%03d.png -r 30 -pix_fmt yuv420p /tmp/out.mp4
