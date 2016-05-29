0793bd7e535c42c80ab9f35d09edf9a1dbb989d6
Michael O'Connor <mkoconnor@gmail.com>
2016-05-29 19:06:45 -0400

    commit

--- app/Main.hs
+++ app/Main.hs
@@ -6,19 +6,12 @@ import Diagrams.Prelude
 import Diagrams.Backend.SVG
 import Diagrams.Backend.SVG.CmdLine
 
--- example :: Diagram B
--- example = unitSquare # fc blue ||| strut (1/4) ||| circle (1/2) # fc yellow
---
--- example :: Diagram B
-
 dollar :: IO (Diagram B)
 dollar = do
    result <- loadImageEmb "/Users/michaeloconnor/Downloads/100-dollar-bill.png"
    return $ case result of
      Left err -> error err
      Right x -> image x # scale 0.05 # rotate (45 @@ deg)
-
--- dollar = loadImageSVG "/Users/michaeloconnor/Downloads/banknote.svg"
 
 handmadeDollar :: Diagram B
 handmadeDollar =
@@ -51,5 +44,11 @@ renderThis i handmadeDollar =
     renderSVG ("/tmp/foo" <> to_3_char_string i <> ".svg") (dims (r2 (600,400))) $ ((handmadeDollar # rotateBy (1 / 60)) <> regPoly 7 3 # fc blue) ||| fromOffsets [r2 (1,1),r2 (1,0),r2 (1/2,1/2)] # strokeLine # lc black # lwL 0.3
     renderThis (i + 1) (handmadeDollar # rotateBy (1/60))
 
+
+grid :: Diagram B
+grid = vsep s $ replicate 4 $ hsep s $replicate 4 handmadeDollar
+  where
+    s = 1
+
 main :: IO ()
-main = renderThis 0 handmadeDollar
+main = mainWith grid
