387e6dfb29df500d037c65d680d2899f5d0fcffc
Michael O'Connor <mkoconnor@gmail.com>
2016-05-29 23:31:09 -0400

    stops

--- app/Main.hs
+++ app/Main.hs
@@ -6,8 +6,14 @@ import Diagrams.Prelude
 import Diagrams.Backend.PGF
 import Diagrams.Backend.PGF.CmdLine
 
-example :: Diagram B
-example = mconcat $ map circle [1..10]
+stop :: [Char] -> Diagram B
+stop s = (square 1 # lw none <> text s) === strutY 1 === circle 0.1 # fc black
+
+finStops :: Diagram B
+finStops = hsep 1 (map (\i -> stop (show i)) [1..5])
+
+omegaStops :: Diagram B
+omegaStops = hsep 1 [finStops, (square 1 # lw none <> text "$\\cdots$"), stop "$\\omega$"]
 
 main :: IO ()
-main = mainWith (example ||| (text "$\\alpha$ and $\\omega$" <> (square 5)))
+main = mainWith omegaStops
